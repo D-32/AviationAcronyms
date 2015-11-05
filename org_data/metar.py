8c82d9a680896f2dd79ff9cfd55ddcedbc29747d
Dylan Marriott <dmarriott@evernote.com>
2015-11-05 21:25:20 +0100

    Removed new line

--- org_data/metar.py
+++ org_data/metar.py
@@ -10,7 +10,7 @@ with open('metar.txt') as f:
   value = ""
   for line in f:
     parts = line.split(';')
-    newData[parts[0]] = parts[1]
+    newData[parts[0]] = parts[1].replace('\n', '')
     
 
 with open('metar_clean.json', 'w') as f:
